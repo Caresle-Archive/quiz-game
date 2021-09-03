1d98f08c162e937db5b89d2fec9351e1f0bce74f
Caresle <carabesle@gmail.com>
2021-09-03 13:43:35 -0600

    Add api route

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,20 @@ require('dotenv').config()
 require('./mongo')
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 const PORT = process.env.PORT || 3001
+
+// routes imports
+const apiRoutes = require('./routes/api.routes')
+
+//middlewares
+app.use(cors())
+app.use(express.json())
+
+// routes
+
+app.use(apiRoutes)
 
 app.listen(PORT, () => {
 	console.log(`Server on port ${PORT}`)
---
+++ backend/src/routes/api.routes.js
@@ -0,0 +1,10 @@
+const { Router } = require('express')
+const route = Router()
+
+const {
+	getAll
+} = require('../controllers/api.controller')
+
+route.get('/api/v1/', getAll)
+
+module.exports = route
