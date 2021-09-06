b4d02363d9848da099451ab5870875798ff0acc4
Caresle <carabesle@gmail.com>
2021-09-05 18:24:39 -0600

    Add fetch data

--- backend/src/controllers/api/questions.controller.js
+++ backend/src/controllers/api/questions.controller.js
@@ -2,7 +2,6 @@ const Question = require('../../models/question')
 
 const getAllQuestions = async (req, res) => {
 	const response = await Question.find()
-	console.log(response)
 	res.json(response)
 }
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import './App.css'
 import CardGroup from './components/CardGroup'
-// import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+const url = 'http://localhost:3001/api/v1/questions'
 
 const App = () => {
-  // const [selected, setSelected] = useState('')
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => setQuestions([...data]))
+  }, [])
+
   return (
     <div>
       <h1>Quiz Game</h1>
