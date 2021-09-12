a849367c2b05bc5452db3ddf5eb97161a67ca816
Caresle <carabesle@gmail.com>
2021-09-11 18:08:06 -0600

    Add cors config

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,13 +4,18 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
+const corsOptions = {
+	origin: '*',
+	exposedHeaders: []
+}
+
 const PORT = process.env.PORT || 3001
 
 // routes imports
 const apiRoutes = require('./routes/api.routes')
 
 //middlewares
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
--- frontend/src/components/ScoreForm.js
+++ frontend/src/components/ScoreForm.js
@@ -1,8 +1,36 @@
 import './ScoreForm.css'
 
 const ScoreForm = () => {
+	const urlScores = "http://localhost:3001/api/v1/test"
+	const handleChange = () => {
+		console.log('handler change')
+	}
+	
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		console.log('hey')
+		const nameInput = document.getElementById('name-input').value
+		const data = {
+			name: nameInput,
+			score_input: 10
+		}
+		fetch(urlScores, {
+			method: 'POST',
+			mode: 'cors',
+			headers: {
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': urlScores
+			},
+			body: JSON.stringify(data)
+		}).then(response => response.json())
+			.then(data => console.log(data))
+	}
+
 	return (
-		<form method="POST" action="http://localhost:3001/api/v1/test">
+		<form method="POST" 
+			action={urlScores}
+			onSubmit={handleSubmit}
+		>
 			<label>Score</label>
 			<input
 				type="text"
@@ -17,6 +45,7 @@ const ScoreForm = () => {
 				hidden
 				value="1"
 				name="score_input"
+				onChange={handleChange}
 			>
 			</input>
 			<button type="submit" className="btn">Add</button>
